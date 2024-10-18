bd23a583d0ccdcb6fa55e6dec0ed50df1c5f256b
Terje Urnes <terje@urnes.net>
2024-10-18 18:49:20 +0200

    Moving pages from the branche

--- webpage/index.js
+++
@@ -1,7 +0,0 @@
-import { ImageViewer } from "../imageviewer.js";
-
-document.addEventListener("readystatechange", (e) => {
-    if (document.readyState === "complete") {
-        new ImageViewer();
-    }
-});
